81ed094bc4039d615d0de64a235df34db9a8a523
mariapaulacastelli <116962666+beescharmer@users.noreply.github.com>
2023-01-27 01:06:32 -0300

    add changes

---
+++ js/main.js
@@ -0,0 +1,40 @@
+function copyText() {
+      
+  /* Copy text into clipboard */
+  navigator.clipboard.writeText
+      (
+        "Hemograma completo"
+        + "\nGlicose"
+        + "\nHemoglobina Glicada"
+        + "\nCreatina com T.F.G"
+        + "\nSódio"
+        + "\nPotássio"
+        + "\nLipidograma completo"
+        + "\nTGO"
+        + "\nTGP"
+        + "\nGama GT"
+        + "\nT4 livre"
+        + "\nTSH"
+        + "\nÁcido Úrico"
+        + "\nFerritina"
+        + "\nSaturação Transferrina"
+        + "\nCPK"
+      );
+
+}
+
+function copyText2() {
+      
+  /* Copy text into clipboard */
+  navigator.clipboard.writeText
+      (
+        "Ecocardiograma"
+        + "\nECG - Eletrocardiograma"
+        + "\nTeste Ergométrico"
+        + "\nHolter 24 horas"
+        + "\nMAPA 24 horas"
+      );
+
+}
+
+
